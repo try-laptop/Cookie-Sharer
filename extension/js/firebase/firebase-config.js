5e4f0779fecf6ed9f52b01f581c7ca5e2dde5d88
try-laptop <try.laptop2024@gmail.com>
2025-05-07 11:46:33 +0530

    Add files via upload

---
+++ extension/js/firebase/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCixpC5gJqVfEI92cPJYRQ6zu5FuvrcAFs",
+  authDomain: "cookie-extension.firebaseapp.com",
+  projectId: "cookie-extension",
+  storageBucket: "cookie-extension.firebasestorage.app",
+  messagingSenderId: "118174618294",
+  appId: "1:118174618294:web:d8d8411f26e00f29aa8c17",
+  measurementId: "G-VJ1WVMPC99",
+  databaseURL: "https://cookie-extension-default-rtdb.firebaseio.com"
+};
